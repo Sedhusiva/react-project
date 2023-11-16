096821bf93819120a39ca02a7dda90f1b24c6628
sedhusiva <sivasanthi.svs@gmail.com>
2023-11-16 07:45:49 +0000

    Script files

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Check the Git branch
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+    # Build your project
+    ./build.sh
+
+    # Log in to Docker Hub (Docker Hub credentials)
+    docker login -u sedhussr -p dckr_pat_qKEqDaqK7TBBQHnXfcgVaaRdQ-M
+
+  
+    # Tag the image
+    docker tag react-appimg sedhussr/dev
+
+    # Push the image to the Dev Docker Hub repository
+    docker push sedhussr/dev
+
+elif [[ $GIT_BRANCH == "origin/main" ]]; then
+    # Build your project
+    ./build.sh
+
+    # Log in to Docker Hub ( Docker Hub credentials)
+    docker login -u sedhussr -p dckr_pat_qKEqDaqK7TBBQHnXfcgVaaRdQ-M
+
+   
+    # Tag the image
+    docker tag react-appimg sedhussr/prod 
+
+    # Push the image to the Prod Docker Hub repository
+    docker push sedhussr/prod
+else
+    echo "Deployment error"
+fi
