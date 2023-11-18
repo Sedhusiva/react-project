768470257e629dc59a401673758f314822d3666f
Sedhusiva <145020056+Sedhusiva@users.noreply.github.com>
2023-11-18 23:21:09 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,25 +3,26 @@
 CURRENT_BRANCH=${GIT_BRANCH}
 
 if [ "$CURRENT_BRANCH" == "origin/main" ]; then
-    # Build your project
-    echo "building for main"
-    . sudo ./build.sh
 
-    # Tag the image
-    sudo docker tag react-appimg sedhussr/prod
+echo "building for main"
 
-    # Push the image to the Dev Docker Hub repository
-    sudo docker push sedhussr/prod
+. ./build.sh
+
+sudo docker tag react-appimg sedhussr/prod
+
+sudo docker push sedhussr/prod
+
 elif [ "$CURRENT_BRANCH" == "origin/dev" ]; then
-    # Build your project
-    echo "building for dev"
-    . sudo ./build.sh
-   
-    # Tag the image
-    sudo docker tag react-appimg sedhussr/dev 
 
-    # Push the image to the Prod Docker Hub repository
-    sudo docker push sedhussr/dev
+echo "building for dev"
+. ./build.sh
+
+sudo docker tag react-appimg sedhussr/dev 
+
+sudo docker push sedhussr/dev
+
 else
-    echo "No action for branch"
+
+echo "No action for branch"
+
 fi
