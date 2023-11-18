7333b507f9b5b2099a752000c144e51d6be9d368
Sedhusiva <145020056+Sedhusiva@users.noreply.github.com>
2023-11-18 19:55:11 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,28 +6,28 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     ./build.sh
 
     # Log in to Docker Hub (Docker Hub credentials)
-    docker login -u sedhussr -p dckr_pat_qKEqDaqK7TBBQHnXfcgVaaRdQ-M
+    sudo docker login -u sedhussr -p dckr_pat_qKEqDaqK7TBBQHnXfcgVaaRdQ-M
 
   
     # Tag the image
-    docker tag react-appimg sedhussr/dev
+    sudo docker tag react-appimg sedhussr/dev
 
     # Push the image to the Dev Docker Hub repository
-    docker push sedhussr/dev
+    sudo docker push sedhussr/dev
 
 elif [[ $GIT_BRANCH == "origin/main" ]]; then
     # Build your project
     ./build.sh
 
     # Log in to Docker Hub ( Docker Hub credentials)
-    docker login -u sedhussr -p dckr_pat_qKEqDaqK7TBBQHnXfcgVaaRdQ-M
+    sudo docker login -u sedhussr -p dckr_pat_qKEqDaqK7TBBQHnXfcgVaaRdQ-M
 
    
     # Tag the image
-    docker tag react-appimg sedhussr/prod 
+    sudo docker tag react-appimg sedhussr/prod 
 
     # Push the image to the Prod Docker Hub repository
-    docker push sedhussr/prod
+    sudo docker push sedhussr/prod
 else
     echo "Deployment error"
 fi
