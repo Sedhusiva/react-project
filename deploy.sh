363ec095f2772f19f8bb66e835b971478e02f374
Sedhusiva <145020056+Sedhusiva@users.noreply.github.com>
2023-11-18 21:04:33 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 # Check the Git branch
-if [ "${GIT_BRANCH}" == "origin/dev" ]; then
+if [ "${BRANCH_NAME}" == "origin/dev" ]; then
     # Build your project
     echo "building for dev"
     . sudo ./build.sh
@@ -12,7 +12,7 @@ if [ "${GIT_BRANCH}" == "origin/dev" ]; then
     # Push the image to the Dev Docker Hub repository
     sudo docker push sedhussr/dev
 
-elif [ "${GIT_BRANCH}" == "origin/main" ]; then
+elif [ "${BRANCH_NAME}" == "origin/main" ]; then
     # Build your project
     echo "building for main"
     . sudo ./build.sh
