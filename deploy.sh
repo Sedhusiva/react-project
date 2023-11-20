9bed8657246c411b8413c7425905070686dbff41
Sedhusiva <145020056+Sedhusiva@users.noreply.github.com>
2023-11-20 13:24:01 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
 
-# Print debug information
-echo "Branch: $GIT_BRANCH"
-echo "Commit: $GIT_COMMIT"
+# Check the Git branch
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+    # Build your project
+    ./build.sh
 
-# Extract branch name from GIT_BRANCH
-BRANCH_NAME=$(echo "$GIT_BRANCH" | awk -F'/' '{print $NF}')
-export BRANCH_NAME
-
-# Check the Jenkins environment variable for Git branch
-if [[ $BRANCH_NAME == "dev" ]]; then
-    # Build and start your project in dev mode
     # Tag the image
     docker tag react-appimg sedhussr/dev
 
     # Push the image to the Dev Docker Hub repository
     docker push sedhussr/dev
 
-elif [[ $BRANCH_NAME == "main" ]]; then
-    # Build and start your project in prod mode
+elif [[ $GIT_BRANCH == "origin/main" ]]; then
+    # Build your project
+    ./build.sh
+
     # Tag the image
     docker tag react-appimg sedhussr/prod 
 
     # Push the image to the Prod Docker Hub repository
     docker push sedhussr/prod
-
 else
-    echo "Deployment error: Unknown branch"
+    echo "Deployment error"
 fi
