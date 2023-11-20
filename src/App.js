97fd4f13b7d28b4c798424f6b01df63236bb8d24
Sedhusiva <145020056+Sedhusiva@users.noreply.github.com>
2023-11-20 15:21:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
             <thead>
               <tr>
                 <th>#reactapp</th>
-                <th>Name</th>
+                <th>Sedhuram S</th>
               </tr>
             </thead>
             <tbody>
