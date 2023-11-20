8f41164fff2c061bd89e1adae81a0fd564bbcd0f
Sedhusiva <145020056+Sedhusiva@users.noreply.github.com>
2023-11-20 12:41:04 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
 
-# Check the Git branch
-if [[ $GIT_BRANCH == "origin/dev" ]]; then
-    # Build your project
-    ./build.sh
+# Print debug information
+echo "Branch: $GIT_BRANCH"
+echo "Commit: $GIT_COMMIT"
 
-    # Log in to Docker Hub (replace with your actual Docker Hub credentials)
-    docker login -u sedhussr -p dckr_pat_qKEqDaqK7TBBQHnXfcgVaaRdQ-M
+# Extract branch name from GIT_BRANCH
+BRANCH_NAME=$(echo "$GIT_BRANCH" | awk -F'/' '{print $NF}')
+export BRANCH_NAME
 
-  
+# Check the Jenkins environment variable for Git branch
+if [[ $BRANCH_NAME == "dev" ]]; then
+    # Build and start your project in dev mode
     # Tag the image
     docker tag react-appimg sedhussr/dev
 
     # Push the image to the Dev Docker Hub repository
     docker push sedhussr/dev
 
-elif [[ $GIT_BRANCH == "origin/main" ]]; then
-    # Build your project
-    ./build.sh
-
-    # Log in to Docker Hub (replace with your actual Docker Hub credentials)
-     docker login -u sedhussr -p dckr_pat_qKEqDaqK7TBBQHnXfcgVaaRdQ-M
-
-   
+elif [[ $BRANCH_NAME == "main" ]]; then
+    # Build and start your project in prod mode
     # Tag the image
     docker tag react-appimg sedhussr/prod 
 
     # Push the image to the Prod Docker Hub repository
     docker push sedhussr/prod
+
 else
-    echo "Deployment error"
+    echo "Deployment error: Unknown branch"
 fi
