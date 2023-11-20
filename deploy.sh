2b492ec60f2b57666671f55560c6f47174f95f40
Sedhusiva <145020056+Sedhusiva@users.noreply.github.com>
2023-11-20 14:22:33 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,15 @@ if [[ $GIT_BRANCH == "dev" ]]; then
 elif [[ $GIT_BRANCH == "main" ]]; then
     # Your main branch logic here
     echo "Building and deploying for main branch"
+     ./build.sh
+    # tag the image
+    docker tag react-appimg sedhussr/prod
+
+    # login credentials
+    docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWD
+
+    # push the image
+    docker push sedhussr/prod
 
 else
     echo "Unsupported branch: $GIT_BRANCH"
