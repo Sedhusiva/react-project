54316199e3f9bf8ff94cbf12f90af795874435d3
Sedhusiva <145020056+Sedhusiva@users.noreply.github.com>
2023-11-20 13:00:35 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Print the value of GIT_BRANCH
+echo "GIT_BRANCH: $GIT_BRANCH"
+
 # Check the Git branch
 if [[ $GIT_BRANCH == "origin/dev" ]]; then
     # Build your project
@@ -8,7 +11,6 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     # Log in to Docker Hub (replace with your actual Docker Hub credentials)
     docker login -u sedhussr -p dckr_pat_qKEqDaqK7TBBQHnXfcgVaaRdQ-M
 
-  
     # Tag the image
     docker tag react-appimg sedhussr/dev
 
@@ -20,14 +22,15 @@ elif [[ $GIT_BRANCH == "origin/main" ]]; then
     ./build.sh
 
     # Log in to Docker Hub (replace with your actual Docker Hub credentials)
-     docker login -u sedhussr -p dckr_pat_qKEqDaqK7TBBQHnXfcgVaaRdQ-M
+    docker login -u sedhussr -p dckr_pat_qKEqDaqK7TBBQHnXfcgVaaRdQ-M
 
-   
     # Tag the image
     docker tag react-appimg sedhussr/prod 
 
     # Push the image to the Prod Docker Hub repository
     docker push sedhussr/prod
+
 else
-    echo "Deployment error"
+    echo "Deployment error: Unsupported branch - $GIT_BRANCH"
 fi
+
