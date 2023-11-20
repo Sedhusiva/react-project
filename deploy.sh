6c778b77d494a833c435235d710f000dd53b3f5d
Sedhusiva <145020056+Sedhusiva@users.noreply.github.com>
2023-11-20 14:38:36 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
 
+# Set the Git branch as an environment variable
+export GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
 # Check the Git branch
-if [[ $GIT_BRANCH == "origin/dev" ]]; then
+if [[ $GIT_BRANCH == "dev" ]]; then
     # Your dev branch logic here
     echo "Building and deploying for dev branch"
-    ./build.sh
-    # tag the image
+     ./build.sh
+
+    # Tag the image
     docker tag react-appimg sedhussr/dev
 
-    # login credentials
+    # docker login credentials
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWD
 
-    # push the image
+    # Push the image to the Prod Docker Hub repository
     docker push sedhussr/dev
 
-elif [[ $GIT_BRANCH == "origin/main" ]]; then
+elif [[ $GIT_BRANCH == "main" ]]; then
     # Your main branch logic here
     echo "Building and deploying for main branch"
-     ./build.sh
-    # tag the image
-    docker tag react-appimg sedhussr/prod
+     # Build your project
+    ./build.sh
 
-    # login credentials
+    # Tag the image
+    docker tag react-appimg sedhussr/prod 
+
+    # docker login credentials
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWD
 
-    # push the image
+    # Push the image to the Prod Docker Hub repository
     docker push sedhussr/prod
 
 else
